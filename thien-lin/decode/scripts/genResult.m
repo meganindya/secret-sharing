060da31504f05768b95f0738783f027922c7be7c
Anindya Kundu <anindya.k22@outlook.com>
2020-05-02 17:08:06 +0530

    Rename directories

---
+++ thien-lin/decode/scripts/genResult.m
@@ -0,0 +1,26 @@
+clear;
+load('.\temp\variables.mat');
+
+A = double(A);
+I = zeros(H, W * K);
+
+for M = 1 : H
+  for N = 1 : W
+    Y = [];
+    for O = 1 : R
+      Y = [Y; A(M, N, O)];
+    end
+    
+    Z = modLagPol(Y, X, 251);
+    Z = Z((R - K + 1) : end);
+    
+    for O = 1 : K
+      I(M, N + (W * (O - 1))) = Z(O);
+    end
+  end
+end
+I = uint8(I);
+
+imwrite(I, 'MESSAGE.jpg');
+
+rmpath('scripts');
---
+++ thien-lin/encode/encode.m
@@ -0,0 +1,42 @@
+clear;
+
+K = 3; N = 5;
+
+A = imread('MESSAGE.jpg');
+[H W] = size(A);
+W = W / K;
+
+B = zeros(H, W, K);
+for P = 1 : H
+  for Q = 1 : W
+    for R = 1 : K
+      B(P, Q, R) = A(P, Q + (W * (R - 1)));
+    end
+  end
+end
+
+C = zeros(H, W, N);
+for P = 1 : H
+  for Q = 1 : W
+    Y = [];
+    for R = 1 : K
+      Y = [Y B(P, Q, R)];
+    end
+    
+    for R = 1 : N
+      C(P, Q, R) = polyval(Y, R);
+      C(P, Q, R) = mod(C(P, Q, R), 251);
+    end
+  end
+end
+C = uint8(C);
+
+D = 'KEYS';
+mkdir(D);
+for R = 1 : N
+  fname = strcat(D, '/', 'K', num2str(R), '.bmp');
+  imwrite(C(:, :, R), fname);
+end
+
+clear;
+disp('-- Keys Successfully Generated');
